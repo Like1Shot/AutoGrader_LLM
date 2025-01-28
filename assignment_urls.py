19f4ccb7570e1031518bb5404ccaadae7b68eb75
Chanheum Park <like1shot@Chanheums-MacBook-Pro.local>
2025-01-28 12:26:08 -0700

    added url file for downloader

---
+++ assignment_urls.py
@@ -0,0 +1,9 @@
+# Course information
+COURSE_NAME = "CSPB-1300"  # Change this to your course name
+
+# Assignment URLs
+ASSIGNMENT_URLS = [
+    "https://applied.cs.colorado.edu/mod/assign/view.php?id=70759&action=grading",
+    #"https://applied.cs.colorado.edu/url2",
+    # Add more URLs as needed
+] 
